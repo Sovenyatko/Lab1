108e6d97ffea40da083eb901930a428dfa5d41a4
Sovenyatko <alexandryshka@ukr.net>
2022-11-11 12:43:03 +0200

    111

---
+++ 12345/12345.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "The war will end and there will be peace\n";
+    std::cout << "Война закончится и будет мир\n";
+}
+
