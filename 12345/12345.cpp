6e51ef2086a13479ffc4d12441966ce82e02cc06
Sovenyatko <alexandryshka@ukr.net>
2022-11-11 12:46:22 +0200

    222
    Змінено рядок 7 та 8

--- 12345/12345.cpp
+++ 12345/12345.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    std::cout << "The war will end and there will be peace\n";
-    std::cout << "Война закончится и будет мир\n";
+    std::cout << "The war will end and there will be peace!!!!!!!\n";
+    std::cout << "Война закончится и будет мир.......\n";
 }
 
